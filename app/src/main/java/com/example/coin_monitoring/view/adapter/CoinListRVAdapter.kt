0389b036be672501630894a1e9749c7cdcb7cc8a
송영현 <dudgus907@naver.com>
2023-06-19 01:59:06 +0900

    dev

--- app/src/main/java/com/example/coin_monitoring/view/adapter/CoinListRVAdapter.kt
+++ app/src/main/java/com/example/coin_monitoring/view/adapter/CoinListRVAdapter.kt
@@ -9,18 +9,20 @@ import com.example.coin_monitoring.R
 import com.example.coin_monitoring.db.entity.InterestCoinEntity
 
 class CoinListRVAdapter(val context : Context, val dataSet : List<InterestCoinEntity>)
-    : RecyclerView.Adapter<CoinListRVAdapter.ViewHolder>(){
+    : RecyclerView.Adapter<CoinListRVAdapter.ViewHolder>() {
 
 
-        inner class ViewHolder(view : View) : RecyclerView.ViewHolder(view)
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
 
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.main_coin_item, parent, false)
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.main_coin_item, parent, false)
 
         return ViewHolder(view)
     }
 
+    //
     override fun getItemCount(): Int {
         return dataSet.size
     }
