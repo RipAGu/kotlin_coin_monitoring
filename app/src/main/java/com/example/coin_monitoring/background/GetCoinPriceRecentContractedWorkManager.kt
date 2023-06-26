6e7c7251809cc1c3fd1b4a8cfd01e86a50fd27ce
송영현 <dudgus907@naver.com>
2023-06-26 11:18:41 +0900

    feat : 주기적 실행함수 추가

--- app/src/main/java/com/example/coin_monitoring/background/GetCoinPriceRecentContractedWorkManager.kt
+++ app/src/main/java/com/example/coin_monitoring/background/GetCoinPriceRecentContractedWorkManager.kt
@@ -1,16 +1,34 @@
 package com.example.coin_monitoring.background
 
 import android.content.Context
+import android.util.Log
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
+import com.example.coin_monitoring.repository.DBRepository
+import kotlinx.coroutines.flow.collect
+import timber.log.Timber
 
 //최근 코인 거래내역 가져오기
 
 class GetCoinPriceRecentContractedWorkManager(val context : Context, workerParameters: WorkerParameters) : CoroutineWorker(context, workerParameters) {
 
+    private val dbRepository = DBRepository()
+
     override suspend fun doWork(): Result {
 
+        getAllInterestSelectedCoinData()
+
         return Result.success()
+    }
+
+    // 1. 관심 목록 클릭한 코인 리스트 가져오기
+    suspend fun getAllInterestSelectedCoinData() {
+
+        dbRepository.getAllInterestSelectedCoinData().collect { selectedCoinList ->
+            for (coinData in selectedCoinList) {
+                Log.d("selectedCoinList", coinData.toString())
+            }
+        }
     }
 
 
